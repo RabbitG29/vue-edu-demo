a60d7852236ae5c80469d6ea55b651125ca421f4
권동현(Kwon Donghyeon)(dhkwon1209) <dhkwon1209@ncsoft.com>
2021-02-26 20:07:06 +0900

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    hello
+    <tool-bar />
+    <router-view />
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue'
 
 export default {
-  methods: {
-    fetchData(){
-      console.log('hello')
-    },
+  components: {
+    ToolBar,
   },
 }
 </script>
