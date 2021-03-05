4526a10839abb554e6cf58d7f63bbc3d47daa3f2
권동현(Kwon Donghyeon)(dhkwon1209) <dhkwon1209@ncsoft.com>
2021-03-05 20:02:30 +0900

    added axios

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     news
+    <div
+      v-for="news in newses"
+      :key="news.id"
+    >
+      {{ news.title }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data(){
+    return {
+      newses: [],
+    }
+  },
+  async created(){
+    this
+    // axio.get은 Promise 객체를 반환함.
+    await axios.get('https://api.hnpwa.com/v0/news/1.json').then((res) => {
+      console.log(res)
+      this.newses = res.data
+    }).catch((err) => {
+      console.log(err)
+    })
+  },
 }
 </script>
 
